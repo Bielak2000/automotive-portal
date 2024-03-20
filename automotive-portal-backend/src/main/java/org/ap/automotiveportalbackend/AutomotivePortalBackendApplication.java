25d987cf0b64c79d00ebdfb4ed3f80bcd4632576
Kacper Bielak <github@bielak2000.com>
2024-03-20 20:12:48 +0100

    Backend - initial commit v2

---
+++ automotive-portal-backend/src/main/java/org/ap/automotiveportalbackend/AutomotivePortalBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ap.automotiveportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotivePortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotivePortalBackendApplication.class, args);
+	}
+
+}
